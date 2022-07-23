8465adeef15241d6908d14164ab238c4ed30f6bd
oneqrhee <blaackpurin@gmail.com>
2022-07-23 15:21:26 +0900

    수정사항

--- src/main/java/com/oneqrhee/post/controller/PostController.java
+++ src/main/java/com/oneqrhee/post/controller/PostController.java
@@ -5,63 +5,41 @@ import com.oneqrhee.post.dto.PostResponseDto;
 import com.oneqrhee.post.dto.PostUpdateRequestDto;
 import com.oneqrhee.post.dto.PostsResponseDto;
 import com.oneqrhee.post.entity.Post;
-import com.oneqrhee.post.repository.PostRepository;
 import com.oneqrhee.post.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 public class PostController {
-    private final PostRepository postRepository;
     private final PostService postService;
 
     @GetMapping("/api/post")
     public List<PostsResponseDto> getPosts() {
-        List<Post> postList = new ArrayList<>(postRepository.findAllByOrderByModifiedAtDesc());
-        List<PostsResponseDto> postsList = new ArrayList<>();
-        for (Post post : postList) {
-            postsList.add(new PostsResponseDto(post));
-        }
-        return postsList;
-    }
-
-    @PostMapping("/api/post")
-    public Post createPost(@RequestBody PostRequestDto postRequestDto) {
-        Post post = new Post(postRequestDto);
-        return postRepository.save(post);
+       return postService.findAllByOrderByModifiedAtDesc();
     }
 
     @GetMapping("/api/post/{id}")
     public PostResponseDto getPost(@PathVariable Long id) {
-        Post post = postRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 Post가 존재하지 않습니다."));
-        return new PostResponseDto(post);
+        return postService.findById(id);
+    }
+
+    @PostMapping("/api/post")
+    public Post createPost(@RequestBody PostRequestDto postRequestDto) {
+        return postService.createPost(postRequestDto);
     }
 
     @PutMapping("/api/post/{id}")
     public ResponseEntity<String> updatePost(@PathVariable Long id, @RequestParam String password,
                                              @RequestBody PostUpdateRequestDto postUpdateRequestDto) {
-        int authorized = postService.checkPassword(id, password);
-        if (authorized == 1) {
-            return new ResponseEntity<>("비밀번호가 틀렸습니다", HttpStatus.UNAUTHORIZED);
-        }
-        postService.updatePost(id, postUpdateRequestDto);
-        return new ResponseEntity<>("글이 수정되었습니다", HttpStatus.OK);
+        return postService.updatePost(id, password, postUpdateRequestDto);
     }
 
     @DeleteMapping("/api/post/{id}")
-    public ResponseEntity<String> deleteCourse(@PathVariable Long id, @RequestParam String password) {
-        int authorized = postService.checkPassword(id, password);
-        if (authorized == 1) {
-            return new ResponseEntity<>("비밀번호가 틀렸습니다", HttpStatus.UNAUTHORIZED);
-        }
-        postRepository.deleteById(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    public ResponseEntity<String> deletePost(@PathVariable Long id, @RequestParam String password) {
+        return postService.deletePost(id, password);
     }
 }
--- src/main/java/com/oneqrhee/post/service/PostService.java
+++ src/main/java/com/oneqrhee/post/service/PostService.java
@@ -1,30 +1,76 @@
 package com.oneqrhee.post.service;
 
+import com.oneqrhee.post.dto.PostRequestDto;
+import com.oneqrhee.post.dto.PostResponseDto;
 import com.oneqrhee.post.dto.PostUpdateRequestDto;
+import com.oneqrhee.post.dto.PostsResponseDto;
 import com.oneqrhee.post.entity.Post;
 import com.oneqrhee.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class PostService {
     private final PostRepository postRepository;
 
-    @Transactional
-    public Long updatePost(Long id, PostUpdateRequestDto postUpdateRequestDto){
-        Post post = postRepository.findById(id).orElseThrow(
-        () ->new IllegalArgumentException("해당 아이디가 존재하지 않습니다"));
-        post.updatePost(postUpdateRequestDto);
-        return post.getId();
+    @Transactional(readOnly = true)
+    public List<PostsResponseDto> findAllByOrderByModifiedAtDesc() {
+        List<Post> postList = new ArrayList<>(postRepository.findAllByOrderByModifiedAtDesc());
+        List<PostsResponseDto> postsResponseDto = new ArrayList<>();
+        for (Post post : postList) {
+            postsResponseDto.add(new PostsResponseDto(post));
+        }
+        return postsResponseDto;
     }
 
-    public int checkPassword(Long id, String password){
+    @Transactional(readOnly = true)
+    public PostResponseDto findById(Long id) {
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 아이디가 존재하지 않습니다."));
+        return new PostResponseDto(post);
+    }
+
+    @Transactional
+    public Post createPost(PostRequestDto postRequestDto){
+        Post post = new Post(postRequestDto);
+        return postRepository.save(post);
+    }
+
+    @Transactional
+    public ResponseEntity<String> updatePost(Long id, String password, PostUpdateRequestDto postUpdateRequestDto) {
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 아이디가 존재하지 않습니다"));
+        int authorized = checkPassword(id, password);
+        if (authorized == 1) {
+            return new ResponseEntity<>("비밀번호가 틀렸습니다", HttpStatus.UNAUTHORIZED);
+        }
+        post.updatePost(postUpdateRequestDto);
+        return new ResponseEntity<>("글이 수정되었습니다", HttpStatus.OK);
+    }
+
+    @Transactional
+    public ResponseEntity<String> deletePost(Long id, String password){
+        int authorized = checkPassword(id, password);
+        if (authorized == 1) {
+            return new ResponseEntity<>("비밀번호가 틀렸습니다", HttpStatus.UNAUTHORIZED);
+        }
+        postRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @Transactional(readOnly = true)
+    public int checkPassword(Long id, String password) {
         int authorized = 0;
         Post post = postRepository.findById(id).orElseThrow(
-                () ->new IllegalArgumentException("해당 아이디가 존재하지 않습니다"));
-        if(!post.getPassword().equals(password)){
+                () -> new IllegalArgumentException("해당 아이디가 존재하지 않습니다"));
+        if (!post.getPassword().equals(password)) {
             authorized = 1;
         }
         return authorized;
